451205591197f1fec4b21aeaf3c7d3e021cb6978
jbpixel <50649423+jbpixel52@users.noreply.github.com>
2022-05-21 16:22:45 -0700

    wip

--- app/my-app/src/components/ResponsiveGrid/appGrid.js
+++ app/my-app/src/components/ResponsiveGrid/appGrid.js
@@ -1,13 +1,28 @@
-
 import AppCard from "../AppCards/AppCard";
 import data from "../../apps.json";
-import './style.css'
+import "./style.css";
+import { Reorder } from "framer-motion";
+import {useState} from 'react';
+
+
+
+let boxes = data.apps.map((item, i) =>
+AppCard(item.name, item.url, item.description)
+);
 
 
 export default function AppsFrame() {
-  let boxes = data.apps.map((item, i) =>
-    AppCard(item.name, item.url, item.description)
+
+
+
+  const [items, setItems] = useState(boxes);
+
+  // return <div className="apps">{boxes}</div>;
+  return (
+    <Reorder.Group axis="y" onReorder={setItems} values={items}>
+      {items.map((item) => (
+        <Item key={item} item={item} />
+      ))}
+    </Reorder.Group>
   );
-  console.log(boxes)
-  return <div className="apps">{boxes}</div>;
 }
